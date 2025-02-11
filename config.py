3739dbd161db8a471fd29b52b79cb0c41ddc1b8f
WeiJin_MUST <69202833+jinweiisgreat@users.noreply.github.com>
2025-02-11 16:31:56 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+# -----------------
+# DATASET ROOTS
+# -----------------
+cifar_10_root = '~/dataset/cifar10'
+cifar_100_root = '~/dataset/cifar100'
+cub_root = '~/dataset/CUB/'
+car_root = '~/dataset/Stanford_cars'
+aircraft_root = '~/dataset/FGVC_Aircraft'
+herbarium_dataroot = '~/dataset/herbarium_19/'
+imagenet_root = '~/dataset/ImageNet100/ILSVRC12'
+pretrained_save_path = '~/pretrained_ovdet/result/pretrained_model_save'
+
+# OSR Split 
+osr_split_dir = './data/ssb_splits'
+#
+# -----------------
+# OTHER PATHS
+# -----------------
+
+dino_pretrain_path = '~/model/pretrained_models/dino/dino_vitbase16_pretrain.pth'
+# -----------------
+# OTHER PATHS
+# -----------------
+exp_root = 'dev_outputs' # All logs and checkpoints will be saved here
